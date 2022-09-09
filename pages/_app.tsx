9bc1758c481a436faa9b49dd44c7fe717bc7f9ba
sakhawat9 <sakhawathossain9769@gmail.com>
2022-09-09 18:22:45 +0600

    add react draftjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import Drift from "react-driftjs";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Drift appId="6h5tuhy2swrh" />
+    </>
+  );
 }
 
 export default MyApp;
