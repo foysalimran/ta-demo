8b71e42c6ef92759970de87525611ad7ec4db071
Foysal Imran <fimran2011@gmail.com>
2024-11-18 12:40:42 +0600

    update

---
+++ pages/essential-woo/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+import Accordion from "../../components/Home/Accordion/Accordion";
+import DemoPage from "../../components/Home/DemoPage/DemoPage";
+import HeroBgAnimated from "../../components/Home/Hero/HeroBgAnimated";
+import Layout from "../../components/Layout";
+import test from "../../public/data.json";
+
+const Home: NextPage = () => {
+  const { essentialWoo }: any = test;
+  const { hero, demo, faq } = essentialWoo[0];  
+
+  return (
+    <Layout
+      title="Event Bundle"
+      description="The Events Calendar Addons Bundle"
+      keywords="	calendar, carousel, elementor, events, grid, layouts, multilingual, no-coding, organizer, plugin, responsive, seo, showcase, venue, wordpress"
+    >
+      <HeroBgAnimated hero={hero} />
+      <DemoPage demo={demo} />
+      <Accordion faq={faq} />
+    </Layout>
+  );
+};
+
+export default Home;
